e0b0bc4822e29a9ccfb85f018519f909a779e6bd
LeeThomas13 <lmtthomasritz@gmail.com>
2020-11-17 16:04:14 -0800

    finished kth value function, tests still need to be written

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -10,6 +10,36 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def insert(self, data):
+        self.head = Node(data)
+
+    def insert_before(self, value, new_value):
+        if not self.head:
+            return
+
+        if self.head == value:
+            self.insert(new_value)
+
+        current = self.head
+
+        while current.next:
+            if current.next.value == value:
+                current.next = Node(new_value, current.next)
+                return
+
+            current = current.next
+
+    def insert_after(self, value):
+        if not self.head:
+            return
+
+        current = self.head
+
+        while current:
+            if current.value == value:
+                current.next = Node(new_value, current.next)
+                return
+
     def append (self, data):
         new_node = Node(data)
         old = self.head
@@ -43,6 +73,36 @@ class LinkedList:
             current = current.next
         result += "Null"
         return result
+
+    # def kth_from_end(self, k):
+    #     list = [1, 3, 8, 2]
+    #     counter = 0
+    #     runner = self.head
+    #     follower = self.head
+    #     while runner.next:
+    #         runner = runner.next
+    #         counter += 1
+    #         if counter > k:
+    #             follower = follower.next
+    #         return follower.data
+    #     if counter > k:
+
+    def kth_from_end(self, k):
+        list = []
+        current = self.head
+        if k < 0:
+            raise valueError("value of k should be positive")
+        count = 0
+        while current != None:
+            if count == k:
+                value_seeking = self.head
+            elif count > k:
+                value_seeking = value_seeking.next
+            current = current.next
+            count+= 1
+        if count > k:
+            return value_seeking.data
+
 
 
 
