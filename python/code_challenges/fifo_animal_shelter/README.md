b0fe2f63d5f26f892babde1217982cc6fcc57512
LeeThomas13 <lmtthomasritz@gmail.com>
2020-11-24 16:24:28 -0800

    finished code challenge 12, all tests passing

--- python/code_challenges/fifo_animal_shelter/README.md
+++ python/code_challenges/fifo_animal_shelter/README.md
@@ -5,7 +5,7 @@ Create a class called AnimalShleter which holds only dogs and cats.
 Make a class method of AnimalShelter which uses enqueue and dequeue. The enqueue(animal) method should add animal to the shelter, and can be either a dog or a cat. dequeue(pref) returns either a dog or a cat. if pref is not dog or cat then return null.
 
 ## Approach & Efficiency
-<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+We approached this challenge by making an animal class, a queue class, and an animal shelter class. the animal class acts as a node class, the queue class makes the data structure, and the animal shelter initializes with a dog queue and a cat queue. Animals are then fed into their respective queue. If you dequeue with a preference you will be given the first animal inputted from its respective queue. If the preference is not dog or cat you will be returned null.
 
 ## Solution
-<!-- Embedded whiteboard image -->
+![fifo animal shelter image](../../assets/fifo-animal-shelter.png)
--- python/code_challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ python/code_challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -1,2 +1,84 @@
+class Node:
+
+    def __init__(self, value =None, next_=None):
+
+        self.value = value
+        self.next_ = next_
+
+
+class Queue:
+
+    def __init__(self, front=None, back=None):
+
+        self.front = front
+        self.back = back
+
+    def enqueue(self, value):
+
+        node = Node(value)
+        if not self.front:
+            self.front = node
+            self.back = node
+            return
+
+        self.back.next_ = node
+        self.back = node
+
+    def dequeue(self):
+
+        if not self.front:
+            raise InvalidOperationError()
+
+        target_node = self.front
+        self.front = target_node.next_
+        return target_node.value
+
+    def peek(self):
+
+        if not self.front:
+            raise InvalidOperationError()
+
+        return self.front.value
+
+    def is_empty(self):
+
+        return not self.front
+
+
 class AnimalShelter:
-    def __init__(self, )
+
+    def __init__(self):
+
+        self.cat_queue = Queue()
+        self.dog_queue = Queue()
+
+    def enqueue(self, value):
+
+        if value == 'cat':
+            new_animal = self.cat_queue.enqueue(value)
+
+
+        elif value == 'dog':
+            self.dog_queue.enqueue(value)
+
+        else:
+            return None
+
+    def  dequeue(self, pref):
+
+        if pref == 'cat':
+            selected_cat = self.cat_queue.dequeue()
+            return selected_cat
+
+        elif pref == 'dog':
+            selected_dog = self.dog_queue.dequeue()
+            return selected_dog
+
+        else:
+            raise InvalidOperationError()
+
+
+class InvalidOperationError(Exception):
+    pass
+
+
