23dac9ac60de83c64a8132676325c76ce7375374
LeeThomas13 <lmtthomasritz@gmail.com>
2020-12-01 13:59:05 -0800

    finished structure of readme and skeleton of test file

---
+++ python/tests/test_tree.py
@@ -0,0 +1,35 @@
+import pytest
+
+from code_challenges.tree import Node, BinaryTree, BinarySearchTree
+
+# Code Challenge 16
+
+def test_make_empty_tree():
+    pass
+
+def test_tree_with_single_root():
+    pass
+
+def test_add_left_and_right():
+    pass
+
+def test_preorder_traversal():
+    pass
+
+def test_inorder_traversal():
+    pass
+
+def test_postorder_traversal():
+    pass
+
+
+# Code Challenge 17
+
+def test_return_desired_value():
+    pass
+
+def test_invalid_value():
+    pass
+
+def test_zero():
+    pass
